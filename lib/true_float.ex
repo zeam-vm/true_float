9f300e28a75ce6acf9af9cbf8a32da8b1d6a3f2b
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-19 10:00:39 +0900

    first commit

---
+++ lib/true_float.ex
@@ -0,0 +1,18 @@
+defmodule TrueFloat do
+  @moduledoc """
+  Documentation for `TrueFloat`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrueFloat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/true_float_test.exs
@@ -0,0 +1,8 @@
+defmodule TrueFloatTest do
+  use ExUnit.Case
+  doctest TrueFloat
+
+  test "greets the world" do
+    assert TrueFloat.hello() == :world
+  end
+end
